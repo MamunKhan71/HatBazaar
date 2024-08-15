3f1ca1185999fb1a2613c7d3f5bc04777d6dac13
Md. Mamun <mkmamun031@gmail.com>
2024-08-15 13:08:57 +0600

    Repolished the card

--- hatbazaar/src/pages/Home/HomePage.jsx
+++ hatbazaar/src/pages/Home/HomePage.jsx
@@ -10,20 +10,25 @@ export default function HomePage() {
     return (
         <div className='space-y-4'>
             <h1 className='text-center font-bold text-3xl'>Products</h1>
-            <div className='border rounded-xl w-full h-full p-4 grid grid-cols-4 gap-4'>
+            <div className='w-full h-full grid grid-cols-4 gap-6 pt-12'>
                 {
                     products?.map(product => (
-                        <div className="card bg-base-100 shadow-xl">
-                            <figure>
+                        <div className="card bg-base-100 shadow-xl rounded-none">
+                            <figure className='relative'>
                                 <img className='h-96 object-cover w-full'
                                     src={product.productImage}
                                     alt="Shoes" />
+                                <span className='absolute bottom-4 left-4 bg-black text-white py-2 px-4 text-xs'>{product.category}</span>
                             </figure>
                             <div className="card-body">
-                                <h2 className="card-title">Shoes!</h2>
-                                <p>If a dog chews shoes whose shoes does he choose?</p>
-                                <div className="card-actions justify-end">
-                                    <button className="btn btn-primary">Buy Now</button>
+                                <h2 className="text-xl font-bold">{product.productName}</h2>
+                                <p className=' truncate'>{product.description}</p>
+                                <p className='font-semibold'>$ {product.price}</p>
+                                <div className="card-actions justify-start">
+                                    <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-black rounded-none hover:bg-black text-white group w-full">
+                                        <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                        <span class="relative w-full transition-colors duration-300 ease-in-out group-hover:text-white text-center">Buy Now</span>
+                                    </a>
                                 </div>
                             </div>
                         </div>
