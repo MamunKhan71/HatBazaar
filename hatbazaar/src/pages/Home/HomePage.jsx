c8fb7c95a6fb5fc3291816459f5258f55fbd7463
Md. Mamun <mkmamun031@gmail.com>
2024-08-17 00:49:07 +0600

    Minor Improvements

--- hatbazaar/src/pages/Home/HomePage.jsx
+++ hatbazaar/src/pages/Home/HomePage.jsx
@@ -25,6 +25,7 @@ export default function HomePage() {
     const numberOfPages = Math.ceil(itemsCount / itemsPerPage)
     const [currentPage, setCurrentPage] = useState(0)
     const pages = [...Array(numberOfPages).keys()]
+    const [loading, setLoading] = useState(true)
     const handleBrandClick = (e) => {
         const { value, checked } = e.target;
         let updatedSelectedBrands = [];
@@ -62,7 +63,7 @@ export default function HomePage() {
                 setFilteredProducts(sortedProducts);
             }
         } else {
-            const sortedProducts = [...filteredProducts].sort((a, b) => new Date(a.creationDateTime) - new Date(b.creationDateTime))
+            const sortedProducts = [...filteredProducts].sort((a, b) => new Date(b.creationDateTime) - new Date(a.creationDateTime))
             setFilteredProducts(sortedProducts)
         }
     }
@@ -89,18 +90,21 @@ export default function HomePage() {
     };
 
     console.log(search);
-
+    const handlePage = (page) => {
+        setCurrentPage(page)
+    }
     useEffect(() => {
         axios.get('https://hatbazaar.vercel.app/count')
             .then(res => setItemsCount(res.data.result))
     }, [])
-    
+
     useEffect(() => {
         axios.get(`https://hatbazaar.vercel.app/products?page=${currentPage}&size=${itemsPerPage}`)
             .then(res => {
                 setProducts(res.data)
                 setFilteredProducts(res.data)
                 setMaxPrice(Math.max(...res.data.map(result => result.price)))
+                setLoading(false)
             })
     }, [currentPage])
 
@@ -197,52 +201,81 @@ export default function HomePage() {
                         </div>
                     </div>
                 </div>
-                <div className='w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 animate__animated animate__fadeIn'>
-                    {
-                        filteredProducts?.map(product => (
-                            <div className="card bg-base-100 shadow-xl rounded-none">
-                                <figure className='relative'>
-                                    <img className='h-96 object-cover w-full'
-                                        src={product.productImage}
-                                        alt="Shoes" />
-                                    <span className='absolute bottom-4 left-4 bg-black text-white py-2 px-4 text-xs'>{product.category}</span>
-                                </figure>
-                                <div className="card-body p-4">
-                                    <div className='flex items-center justify-between'>
-                                        <h2 className="text-xl font-bold">{product.productName}</h2>
-                                        <h2 className="text-sm font-bold inline-flex gap-2 items-center"><FaStar className='text-yellow-400' />{product.ratings}</h2>
+                {
+                    loading ? <div className='flex w-full justify-center items-center'>
+                        <div className='w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 animate__animated animate__fadeIn'>
+                            {([...Array(10)].map(item => <>
+                                <div role="status" class="p-4 rounded shadow md:p-6 ">
+                                    <div class="flex items-center justify-center h-96 mb-4 bg-gray-300 rounded">
+
                                     </div>
-                                    <p className=' truncate'>{product.description}</p>
-                                    <div className='flex justify-between items-center'>
-                                        <p className='font-semibold'>$ {product.price}</p>
-                                        <span className="font-semibold text-right inline-flex gap-2 items-center">
-                                            <FaRegCalendarAlt />
-                                            {new Date(product.creationDateTime).toLocaleDateString('en-US', {
-                                                month: 'short',
-                                                day: 'numeric',
-                                                year: 'numeric',
-                                            })}
-                                        </span>
+                                    <div class="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
+                                    <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                    <div class="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                    <div class="h-2 bg-gray-200 rounded-full"></div>
+                                    <div class="flex items-center mt-4">
+                                        <div>
+                                            <div class="h-2.5 bg-gray-200 rounded-full w-32 mb-2"></div>
+                                            <div class="w-48 h-2 bg-gray-200 rounded-full "></div>
+                                        </div>
                                     </div>
-                                    <div className="card-actions justify-start">
-                                        <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-black rounded-none hover:bg-black text-white group w-full">
-                                            <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                                            <span class="relative w-full transition-colors duration-300 ease-in-out group-hover:text-white text-center">Buy Now</span>
-                                        </a>
-                                    </div>
+                                    <span class="sr-only">Loading...</span>
                                 </div>
-                            </div>
-                        ))
-                    }
-                </div>
+
+                            </>))}
+                        </div>
+                    </div>
+                        :
+                        <div className='w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 animate__animated animate__fadeIn'>
+                            {
+                                filteredProducts?.map(product => (
+                                    <div className="card bg-base-100 shadow-xl rounded-none">
+                                        <figure className='relative'>
+                                            <img className='h-96 object-cover w-full'
+                                                src={product.productImage}
+                                                alt="Shoes" />
+                                            <span className='absolute bottom-4 left-4 bg-black text-white py-2 px-4 text-xs'>{product.category}</span>
+                                        </figure>
+                                        <div className="card-body p-4">
+                                            <div className='flex items-center justify-between'>
+                                                <h2 className="text-xl font-bold">{product.productName}</h2>
+                                                <h2 className="text-sm font-bold inline-flex gap-2 items-center"><FaStar className='text-yellow-400' />{product.ratings}</h2>
+                                            </div>
+                                            <p className=' truncate'>{product.description}</p>
+                                            <div className='flex justify-between items-center'>
+                                                <p className='font-black'>$ {product.price}</p>
+                                                <span className="font-semibold text-gray-500 text-right inline-flex gap-2 items-center">
+                                                    <FaRegCalendarAlt />
+                                                    {new Date(product.creationDateTime).toLocaleDateString('en-US', {
+                                                        month: 'short',
+                                                        day: 'numeric',
+                                                        year: 'numeric',
+                                                        hour: 'numeric',
+                                                        minute: 'numeric'
+                                                    })}
+                                                </span>
+                                            </div>
+                                            <div className="card-actions justify-start">
+                                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-black rounded-none hover:bg-black text-white group w-full">
+                                                    <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                                    <span class="relative w-full transition-colors duration-300 ease-in-out group-hover:text-white text-center">Buy Now</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                }
+
             </div>
             <div className='flex w-full items-center justify-end pt-12'>
                 <div className="join">
-                    <button onClick={handlePrevious} className="join-item btn">«</button>
+                    <button onClick={handlePrevious} className="join-item btn rounded-none">«</button>
                     {
-                        pages?.map(page => <button className="join-item btn">{page + 1}</button>)
+                        pages?.map(page => <button onClick={() => handlePage(page)} className={`join-item btn rounded-none ${page === currentPage && 'bg-black text-white'}`}>{page + 1}</button>)
                     }
-                    <button onClick={handleNext} className="join-item btn">»</button>
+                    <button onClick={handleNext} className="join-item btn rounded-none">»</button>
                 </div>
             </div>
         </div>
