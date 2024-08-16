513475d8cad8093926ddd658092ce07d492747d6
Md. Mamun <mkmamun031@gmail.com>
2024-08-16 08:56:37 +0600

    Added Price Based Filtering Technique

--- hatbazaar/src/pages/Home/HomePage.jsx
+++ hatbazaar/src/pages/Home/HomePage.jsx
@@ -6,6 +6,7 @@ import { TbCalendarCode } from "react-icons/tb";
 import { BiFilter } from "react-icons/bi";
 import { TbBrandAbstract } from "react-icons/tb";
 import { BiCategoryAlt } from "react-icons/bi";
+import 'animate.css';
 import axios from 'axios';
 export default function HomePage() {
     const [products, setProducts] = useState([])
@@ -14,42 +15,34 @@ export default function HomePage() {
     const [brands, setBrands] = useState([])
     const [selectedBrands, setSelectedBrands] = useState([])
     const [selectedCategory, setSelectedCategory] = useState([])
+    const [price, setPrice] = useState(0)
+    const [maxPrice, setMaxPrice] = useState(0)
     const handleBrandClick = (e) => {
         const { value, checked } = e.target;
         let updatedSelectedBrands = [];
-        
         if (checked) {
             updatedSelectedBrands = [...selectedBrands, value];
         } else {
             updatedSelectedBrands = selectedBrands.filter((brand) => brand !== value);
         }
-        
         setSelectedBrands(updatedSelectedBrands);
-        filterProducts(updatedSelectedBrands, selectedCategory);
+        filterProducts(updatedSelectedBrands, selectedCategory, maxPrice);
     };
-    
+
     const handleCategoryClick = (e) => {
         const { value, checked } = e.target;
         let updatedCategory = [];
-        
+
         if (checked) {
             updatedCategory = [...selectedCategory, value];
         } else {
             updatedCategory = selectedCategory.filter(category => category !== value);
         }
-        
+
         setSelectedCategory(updatedCategory);
-        filterProducts(selectedBrands, updatedCategory);
+        filterProducts(selectedBrands, updatedCategory, maxPrice);
     };
-    
-    const filterProducts = (brands, categories) => {
-        const filtered = products.filter(product =>
-            (brands.length === 0 || brands.includes(product.brandName)) &&
-            (categories.length === 0 || categories.includes(product.category))
-        );
-        
-        setFilteredProducts(filtered);
-    };
+
     console.log(selectedCategory);
     const filterBy = (type, sortType) => {
         if (type === "price") {
@@ -66,13 +59,31 @@ export default function HomePage() {
         }
     }
     console.log(filteredProducts);
+    const handlePrice = (e) => {
+        const newPrice = Number(e.target.value)
+        const finalPrice = newPrice + 25
+        setPrice(finalPrice)
+        filterProducts(selectedBrands, selectedCategory, price);
+    }
+    const filterProducts = (brands, categories, maxPrice) => {
+        const filtered = products.filter(product =>
+            (brands.length === 0 || brands.includes(product.brandName)) &&
+            (categories.length === 0 || categories.includes(product.category)) &&
+            (maxPrice === 0 || product.price <= maxPrice)
+        );
+
+        setFilteredProducts(filtered);
+    };
     useEffect(() => {
         axios.get('http://localhost:5000/products')
             .then(res => {
                 setProducts(res.data)
                 setFilteredProducts(res.data)
+                setMaxPrice(Math.max(...res.data.map(result => result.price)))
             })
     }, [])
+
+    console.log(price);
     useEffect(() => {
         setCategory([...new Set(products.map((product) => product.category))]);
         setBrands([...new Set(products.map(product => product.brandName))])
@@ -81,7 +92,20 @@ export default function HomePage() {
     return (
         <div className='space-y-4'>
             <h1 className='text-center font-bold text-3xl'>Products</h1>
-            <div className='w-full flex justify-end items-center'>
+            <div className='w-full flex gap-5 justify-end items-center'>
+                <label className="input bg-base-200 flex items-center gap-2 rounded-none">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                            fillRule="evenodd"
+                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                            clipRule="evenodd" />
+                    </svg>
+                    <input type="text" className="grow" placeholder="Search..." />
+                </label>
                 <details className="dropdown dropdown-end">
                     <summary className="btn m-1 rounded-none"><RxDropdownMenu /> Sort by</summary>
                     <ul className="menu dropdown-content bg-base-100 z-[1] w-52 p-2 rounded-none shadow font-semibold">
@@ -97,13 +121,13 @@ export default function HomePage() {
                     <div className='bg-gray-50 p-4 space-y-2'>
                         <p className='inline-flex gap-2 items-center font-semibold'><MdOutlineAttachMoney />Price</p>
                         <div>
-                            <input type="range" min={0} max="100" value="25" className="range range-xs" step="50" />
+                            <input onChange={(e) => handlePrice(e)} type="range" min={0} max={maxPrice} value={price} className="range range-xs" step="25" />
                             <div className="flex w-full justify-between px-2 text-xs">
+                                <span>0</span>
                                 <span>|</span>
                                 <span>|</span>
                                 <span>|</span>
-                                <span>|</span>
-                                <span>|</span>
+                                <span>{maxPrice}</span>
                             </div>
                         </div>
                     </div>
@@ -140,7 +164,7 @@ export default function HomePage() {
                         </div>
                     </div>
                 </div>
-                <div className='w-full h-full grid grid-cols-3 gap-6'>
+                <div className='w-full h-full grid grid-cols-3 gap-6 animate__animated animate__fadeIn'>
                     {
                         filteredProducts?.map(product => (
                             <div className="card bg-base-100 shadow-xl rounded-none">
